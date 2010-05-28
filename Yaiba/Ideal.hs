c1c57c03c0c638f0e9a055ea0215a3e2914da4f9
Jeremy Ong <jco34@cornell.edu>
2010-05-28 15:33:46 -0400

    Changed directory structure so cabal finds everything properly

--- Yaiba/Ideal.hs
+++ Yaiba/Ideal.hs
@@ -6,12 +6,12 @@ module Yaiba.Ideal where
 import Data.Array.Repa as R
 import Data.Array.Repa.Index
 import Data.Array.Repa.Shape
+import Data.Array.Parallel.Unlifted as U
 import Yaiba.Monomial
 import Yaiba.Polynomial
 
 newtype Ideal ord = Ideal (R.Array DIM1 (Polynomial ord))
 
-printIdeal :: (Show a, Shape sh,
-      dph-prim-par-0.4.0:Data.Array.Parallel.Unlifted.Elt a) =>
-     Array sh a -> String
+printIdeal :: (Show a, Shape sh, U.Elt a) =>
+     R.Array sh a -> String
 printIdeal a = Prelude.show $ R.toList a
