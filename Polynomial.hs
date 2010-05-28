54a52e0d2b837be4862d2bcd27749727294a53ef
Jeremy Ong <jco34@cornell.edu>
2010-05-27 23:49:20 -0400

    Polynomials can now be added and multiplied to produce other polynomials. TODO: Wrap the map values as Maybe types to easily prune monomials with coefficients equal to zero.

--- Polynomial.hs
+++ Polynomial.hs
@@ -1,5 +1,5 @@
 
-{-# OPTIONS_GHC -fglasgow-exts #-}
+{-# OPTIONS_GHC -fglasgow-exts -XUndecidableInstances #-}
 
 module Yaiba.Polynomial where
 
@@ -10,16 +10,28 @@ import Prelude hiding (null)
 
 newtype Polynomial ord = Polynomial (Map (Monomial ord) Q)
 
-instance Show (Polynomial Lex) where
-  show (Polynomial a) = showTerm $ reverse (toAscList (a::Map (Monomial Lex) Q))
-    where showTerm [] = ""
-          showTerm (a:[]) = show a 
-          showTerm (a:as) = (show a) ++ " + " ++ (showTerm as)
-          
+instance Show (Polynomial ord) where
+  show (Polynomial a) = showTerm $ toList a where
+    showTerm [] = ""                    
+    showTerm (a:[]) = show a
+    showTerm (a:as) = (show a) ++ " + " ++ (showTerm as)
+
+-- A shortened prettyLexPrint
+pLp :: Polynomial Lex -> [Char]
+pLp = prettyLexPrint
+
+prettyLexPrint :: Polynomial Lex -> [Char]
+prettyLexPrint (Polynomial a) = showTerm $ reverse (toAscList (a::Map (Monomial Lex) Q)) where 
+  showTerm [] = ""
+  showTerm (a:[]) = show a 
+  showTerm (a:as) = (show a) ++ " + " ++ (showTerm as)                        
+
 --An empty polynomial.
 nullPoly :: Polynomial ord
 nullPoly = Polynomial empty
           
+monPoly a b = Polynomial (singleton a b)
+           
 --Dummy instance. Don't use, use "compare" instead
 instance Eq (Polynomial ord) where
   
@@ -37,9 +49,13 @@ instance Ord (Polynomial Lex) where
 insertTerm :: (Ord (Monomial ord)) => Polynomial ord -> Monomial ord -> Q -> Polynomial ord
 insertTerm (Polynomial a) b c = Polynomial (insertWith (+) b c a)
 
-instance (Ord (Monomial ord)) => Num (Polynomial ord) where
-  (Polynomial a) + (Polynomial b) = Polynomial $ unionWith (+) a b
+getMap :: Polynomial ord -> Map (Monomial ord) Q
+getMap (Polynomial a) = a
 
---addCoefficients :: Set (Term ord) -> Q
---addCoefficients a = fold f 0 a where
-  --f = (+) . coefficient
+instance (Ord (Monomial ord)) => Num (Polynomial ord) where
+  Polynomial a + Polynomial b = Polynomial $ unionWith (+) a b
+  a * Polynomial b = Polynomial $ foldWithKey (\k v -> unionWith (+) (getMap (monMult k v a))) empty b
+    
+monMult :: (Ord (Monomial ord)) => Monomial ord -> Q -> Polynomial ord -> Polynomial ord
+monMult a b (Polynomial c) = Polynomial $ foldWithKey (f a b) empty c where
+  f a b k v acc = unionWith (+) (singleton (a*k) (b*v)) acc
