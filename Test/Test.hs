ff8389c96cc41eab555bbfa8e0ae7b45016d9818
Jeremy Ong <jco34@cornell.edu>
2010-06-11 20:56:49 -0400

    Test adjusted to match change in implementation

--- Test/Test.hs
+++ Test/Test.hs
@@ -7,8 +7,9 @@ import Yaiba.Buchberger
 import Yaiba.Ideal
 import Yaiba.Polynomial
 import Yaiba.Monomial
+import qualified Data.Map as M
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
-
+{-
 -- f = xy^2 + 1
 f = fromList [(Monomial [1,2],1),
               (Monomial [0,0],1)]
@@ -36,23 +37,28 @@ g_1 = f_1
 
 -- g_2 = y^2-1
 g_2 = f'_2
-
+-}
 h_1 = fromList [(Monomial [3,0],1),
                 (Monomial [1,1],-2)]
 h_2 = fromList [(Monomial [2,1],1),
                 (Monomial [0,2],-2),
                 (Monomial [1,0],1)]
+{-
 h'_1 = fromList [(Monomial [3,0],1),
                 (Monomial [1,1],-2)]
 h'_2 = fromList [(Monomial [2,1],1),
                 (Monomial [0,2],-2),
                 (Monomial [1,0],1)]
-
-h = Ideal [h_1,h_2] :: Ideal Grlex
-h' = Ideal [h'_1,h'_2] :: Ideal Lex
-
+-}
+h = Ideal $ M.fromList [(h_1,fst (leadTerm h_1)),(h_2,fst (leadTerm h_2))] :: Ideal Grlex
+--h' = Ideal [h'_1,h'_2] :: Ideal Lex
+{-
 i = Ideal [f_1,f,f_2,f',f'_1,g,g_1,g_2,h_1,h_2] :: Ideal Grlex
 
+j_1 = fromList [(Monomial [0,0,2,1],3),(Monomial [0,0,1,2],-1),(Monomial [1,0,0,0],1)]
+j_2 = fromList [(Monomial [0,0,3,2],2),(Monomial [0,0,1,4],-3),(Monomial [0,1,0,0],1)]
+j = Ideal [j_1,j_2] :: Ideal Grlex
+-}
 main = do 
 {-  -- Example 1 from CLO \S2.3
   putStrLn $ "f/[f_1,f_2] produces remainder " ++ (pLp $ f /. (Ideal [f_1,f_2]))
@@ -65,10 +71,10 @@ main = do
   putStrLn $ "g/[g_2,g_1] prodcues remainder " ++ (pLp $ g /. (Ideal [g_2,g_1]))-}
   -- Example 1 from CLO \S2.7
   start <- getCurrentTime
-  putStrLn $ "A (parallel) non-reduced GB of h is " ++ (show (getPolys $ gB i))
+  putStrLn $ "A (parallel) non-reduced GB of h is " ++ (show (getPolys $ gB h))
   end <- getCurrentTime
-  putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."
+  putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."{-
   start' <- getCurrentTime
-  putStrLn $ "A non-reduced GB of h is " ++ (show (getPolys $ nPgB i))
+  putStrLn $ "A non-reduced GB of h is " ++ (show (getPolys $ nPgB h))
   end' <- getCurrentTime
-  putStrLn $ show (end' `diffUTCTime` start') ++ " elapsed."
+  putStrLn $ show (end' `diffUTCTime` start') ++ " elapsed."-}
