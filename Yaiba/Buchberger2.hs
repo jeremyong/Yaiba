37bf6224c3ee4f71c0ef476ebc1e6eb2ce68f7d2
Jeremy Ong <jco34@cornell.edu>
2010-06-15 18:12:20 -0400

    Brief

---
+++ Yaiba/Buchberger2.hs
@@ -0,0 +1,11 @@
+
+module Yaiba.Buchberger2 where
+
+import Yaiba.Monomial
+import Yaiba.Polynomial
+import Yaiba.Ideal2
+import Yaiba.Sugar2
+import Control.Parallel
+import Control.Parallel.Strategies
+import Prelude hiding (rem,null,map,filter)
+
