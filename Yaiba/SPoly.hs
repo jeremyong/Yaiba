bf2313eca4028fd01ddefdc2f90a812b096f2afa
Jeremy Ong <jco34@cornell.edu>
2010-06-15 21:03:42 -0400

    Wrote sPoly function to also compute Sugars

--- Yaiba/SPoly.hs
+++ Yaiba/SPoly.hs
@@ -1,18 +1,22 @@
-{-# OPTIONS_GHC -fdph-par #-}
+{-# OPTIONS_GHC -fdph-par -fglasgow-exts -XUndecidableInstances #-}
 {-# LANGUAGE PArr #-}
 module Yaiba.SPoly where
 
-import Data.Map
+import qualified Data.Map as DM
 --import Data.Array.Parallel.Prelude
 import Yaiba.Sugar
+import Yaiba.Monomial
 import Yaiba.Polynomial
 
-newtype SPoly ord = SP (Map (Sugar ord) [:Poly ord:])
+newtype SPoly ord = SP (DM.Map (Sugar ord) [:Poly ord:])
 
-sPoly (a,a') (b,b') = let (a1,a2) = leadTerm a
-                          (b1,b2) = leadTerm b
-                          l = lcmMon a1 b1
-                      in (monMult (l/a1) (b2/a2) a) - (monMult (l/b1) 1 b)
+sPoly :: (Ord (Mon ord)) => (Poly ord,Sugar ord) -> 
+         (Poly ord, Sugar ord) -> (Poly ord, Sugar ord)
+sPoly (a,S (a1,a2,a3)) (b,S (b1,b2,b3)) = let l = lcmMon a1 b1
+                                              sp = (monMult (l/a1) (b2/a2) a) - (monMult (l/b1) 1 b)
+                                              (spLT,co) = leadTerm sp
+                                              spLTdeg = degree spLT
+                                              sug = spLTdeg + max (a3-spLTdeg) (b3-spLTdeg)
+                                          in (sp,S (spLT,co,sug))
 
-syzygy [] _ = SP empty
-syzygy as b = map (\x -> 
+syzygy as b = map (sPoly b) as
--- Yaiba/Sugar.hs
+++ Yaiba/Sugar.hs
@@ -3,14 +3,15 @@
 module Yaiba.Sugar where
 
 import Yaiba.Monomial
+import Math.Algebra.Field.Base
 
 --Sugar tuples consist of the LT and the sugar itself
-newtype Sugar ord = S ((Mon ord), Int)
+newtype Sugar ord = S (Mon ord,Q,Int)
 
 instance Eq (Sugar ord) where --Dummy instance, use compare instead
 
 instance (Ord (Mon ord)) => Ord (Sugar ord) where
-  compare (S (a,_)) (S (b,_)) = compare a b
+  compare (S (a,_,_)) (S (b,_,_)) = compare a b
   
 instance (Ord (Mon ord)) => Show (Sugar ord) where
-  show (S (a,_)) = show a
+  show (S (a,_,_)) = show a
