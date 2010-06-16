bd176e2f7b90a6c05dfb6227bdb4030a001f8911
Jeremy Ong <jco34@cornell.edu>
2010-06-15 22:12:08 -0400

    syzygy currently prunes relatively prime s-pairs

--- Yaiba/SPoly.hs
+++ Yaiba/SPoly.hs
@@ -3,7 +3,7 @@
 module Yaiba.SPoly where
 
 import qualified Data.Map as DM
---import Data.Array.Parallel.Prelude
+import Data.Array.Parallel.Prelude
 import Yaiba.Sugar
 import Yaiba.Monomial
 import Yaiba.Polynomial
@@ -11,12 +11,17 @@ import Yaiba.Polynomial
 newtype SPoly ord = SP (DM.Map (Sugar ord) [:Poly ord:])
 
 sPoly :: (Ord (Mon ord)) => (Poly ord,Sugar ord) -> 
-         (Poly ord, Sugar ord) -> (Poly ord, Sugar ord)
+         (Poly ord, Sugar ord) -> Maybe (Poly ord, Sugar ord)
 sPoly (a,S (a1,a2,a3)) (b,S (b1,b2,b3)) = let l = lcmMon a1 b1
                                               sp = (monMult (l/a1) (b2/a2) a) - (monMult (l/b1) 1 b)
                                               (spLT,co) = leadTerm sp
                                               spLTdeg = degree spLT
                                               sug = spLTdeg + max (a3-spLTdeg) (b3-spLTdeg)
-                                          in (sp,S (spLT,co,sug))
+                                          in case a1 * b1 == l of
+                                            False -> Just (sp,S (spLT,co,sug))
+                                            True -> Nothing
 
-syzygy as b = (map (sPoly b) as)
+
+syzygy as b = SP $ foldl (\x y -> f (sPoly b y) x) DM.empty as where
+  f Nothing acc = acc
+  f (Just (sp,sug)) acc = DM.insertWith (+:+) sug [:sp:] acc
