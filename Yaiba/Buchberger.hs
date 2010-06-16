d4170ca53d9ba3239d05a3f4bc36ff3dfce8d63d
Jeremy Ong <jco34@cornell.edu>
2010-06-16 13:33:55 -0400

    Reformatting and typos

--- Yaiba/Buchberger.hs
+++ Yaiba/Buchberger.hs
@@ -6,19 +6,17 @@ module Yaiba.Buchberger where
 import Yaiba.Monomial
 import Yaiba.Polynomial
 import Yaiba.Ideal
-import Yaiba.Sugar
 import Yaiba.SPoly
-import Data.Map hiding (null,filter,map)
-import Data.List
-import Data.Array.Parallel.Prelude hiding (not)
+import Data.Map hiding (filter,map)
+import Data.List hiding (null)
 import Prelude hiding (rem,null,map,filter)
 
 gB :: (Ord (Mon ord)) => Ideal ord -> Ideal ord
 gB a = gB' a (getSPolys (I []) a) where
-  gB' b (SP empty) = b
-  gB' d@(I ds) (SP spolys) = let ((_,polys),rest) = deleteFindMin spolys
-                                 redPolys = filter (not . isNull) $ map (\x -> x /. d) polys
-                                 initRed = initSugars redPolys
-                                 SP new = getSPolys d (I initRed)
-                                 nextSMap = SP $ unionWith (++) spolys new
-                             in gB' (I $ ds++initRed) nextSMap
+  gB' d@(I ds) (SP spolys) | null spolys = d
+                           | otherwise = let ((_,polys),rest) = deleteFindMin spolys
+                                             redPolys = filter (not . isNull) $ map (\x -> x /. d) polys
+                                             initRed = initSugars redPolys
+                                             SP new = getSPolys d (I initRed)
+                                             nextSMap = SP $ unionWith (++) rest new
+                                         in gB' (I $ ds++initRed) nextSMap
