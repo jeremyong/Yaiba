3dc05f8409a51c6765120bd5017af1a44c6077db
syzygic <fmoore@gmail.com>
2010-06-15 23:22:13 -0400

    Removed minimal syzygy code - was incorrect.

--- Yaiba/SPoly.hs
+++ Yaiba/SPoly.hs
@@ -12,21 +12,15 @@ import Yaiba.Ideal
 newtype SPoly ord = SP (DM.Map (Sugar ord) [:Poly ord:])
 
 sPoly :: (Ord (Mon ord)) => (Poly ord,Sugar ord) -> 
-         (Poly ord, Sugar ord) -> [Mon ord] -> Maybe (Poly ord, Sugar ord)
-sPoly (a,S (a1,a2,a3)) (b,S (b1,b2,b3)) ms = let l = lcmMon a1 b1
-                                                 sp = (monMult (l/a1) b2 a) - (monMult (l/b1) a2 b)
-                                                 (spLT,co) = leadTerm sp
-                                                 spLTdeg = degree spLT
-                                                 sug = spLTdeg + max (a3-spLTdeg) (b3-spLTdeg)
-                                             in case a1 * b1 == l of
-                                               False -> in case isMinimal ms spLT of
-                                                          True -> Just (sp,S (spLT,co,sug))
-                                                          False -> Nothing
-                                               True -> Nothing
-
-isMinimal :: (Ord (Mon ord)) =>
-             [Mon ord] -> Mon ord -> Boolean
-isMinimal [] _ = True
+         (Poly ord, Sugar ord) -> Maybe (Poly ord, Sugar ord)
+sPoly (a,S (a1,a2,a3)) (b,S (b1,b2,b3)) = let l = lcmMon a1 b1
+                                              sp = (monMult (l/a1) b2 a) - (monMult (l/b1) a2 b)
+                                              (spLT,co) = leadTerm sp
+                                              spLTdeg = degree spLT
+                                              sug = spLTdeg + max (a3-spLTdeg) (b3-spLTdeg)
+                                          in case a1 * b1 == l of
+                                            False -> Just (sp,S (spLT,co,sug))
+                                            True -> Nothing
 
 syzygy :: (Ord (Mon ord)) =>
           Ideal ord -> (Poly ord, Sugar ord) -> DM.Map (Sugar ord) [:Poly ord:]
