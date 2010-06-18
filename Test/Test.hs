a563917d6e85c80842f4c635616dd54a9b3091d2
Frank Moore <frankmoore@habanero.math.cornell.edu>
2010-06-18 18:52:59 -0400

    Changed test.

--- Test/Test.hs
+++ Test/Test.hs
@@ -44,7 +44,7 @@ f = "output"
 
 main = do 
   --let gb = show $ (getPolys $ gB j)
-  let gb = show $ (leadTerm $ last $ getPolys $ gB'' p 3)
+  let gb = show $ (leadTerm $ last $ getPolys $ gB'' p 2)
   putStrLn "8"
   putStrLn ("Lex")
   --putStrLn (show (getPolys j))
