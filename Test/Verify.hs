37199b9362a0cfdbc8a0cb99b90e2e74266b0716
syzygic <fmoore@gmail.com>
2010-07-12 11:33:15 -0400

    Example of bad behavior.

--- Test/Verify.hs
+++ Test/Verify.hs
@@ -18,20 +18,27 @@ import qualified Data.List as DL
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import System.IO
 
-j_1 = P.fromList [(M.fromList [5,3,4,3,0,0,0,0],-1),(M.fromList [0,0,0,0,1,0,0,0],1)]
-j_2 = P.fromList [(M.fromList [4,3,6,3,0,0,0,0],-1),(M.fromList [0,0,0,0,0,1,0,0],1)]
+j_1 = P.fromList [(M.fromList [2,3,4,3,0,0,0,0],-1),(M.fromList [0,0,0,0,1,0,0,0],1)]
+j_2 = P.fromList [(M.fromList [4,3,2,3,0,0,0,0],-1),(M.fromList [0,0,0,0,0,1,0,0],1)]
 j_3 = P.fromList [(M.fromList [3,5,3,5,0,0,0,0],-1),(M.fromList [0,0,0,0,0,0,1,0],1)]
 j_4 = P.fromList [(M.fromList [2,3,4,3,0,0,0,0],-1),(M.fromList [0,0,0,0,0,0,0,1],1)]
 j' = j_1:j_2:j_3:j_4:[]
 j = DS.fromList (DL.map (\x-> P.PS x) (initSugars j'))
 jideal = I $ DV.fromList (initSugars j') :: Ideal M.Lex
 
-p_1 = P.fromList [(M.fromList [4,0,0,0],1),(M.fromList [0,1,0,0],-1)]
+{-p_1 = P.fromList [(M.fromList [4,0,0,0],1),(M.fromList [0,1,0,0],-1)]
 p_2 = P.fromList [(M.fromList [3,0,0,0],1),(M.fromList [0,0,1,0],-1)]
 p_3 = P.fromList [(M.fromList [2,0,0,0],1),(M.fromList [0,0,0,1],-1)]
 p' = p_1:p_2:p_3:[]
-p = DS.fromList (initSugars p')
-pideal = I $ DV.fromList (initSugars p') :: Ideal M.Grlex
+p = DS.fromList (DL.map (\x-> P.PS x) (initSugars p'))
+pideal = I $ DV.fromList (initSugars p') :: Ideal M.Lex-}
+
+p_1 = P.fromList [(M.fromList [0,0,1,0,0],1),(M.fromList [1,0,0,0,0],-1),(M.fromList [0,1,0,0,0],-1)]
+p_2 = P.fromList [(M.fromList [0,0,0,1,0],1),(M.fromList [2,0,0,0,0],-1),(M.fromList [1,1,0,0,0],-2)]
+p_3 = P.fromList [(M.fromList [0,0,0,0,1],1),(M.fromList [3,0,0,0,0],-1),(M.fromList [2,1,0,0,0],-3)]
+p' = p_1:p_2:p_3:[]
+p = DS.fromList (DL.map (\x-> P.PS x) (initSugars p'))
+pideal = I $ DV.fromList (initSugars p') :: Ideal M.Lex
 
 main = do 
   let gb = show $ (getPolys $ gB j)
